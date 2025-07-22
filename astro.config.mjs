231543dc8501ae8499e4a6a41cfcf10327a0f5f8
Luca Mason <lucarmsn@gmail.com>
2025-07-21 22:16:19 -0400

    second update for launching site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const SERVER_PORT = 1234;
+const LOCALHOST_URL = 'https://localhost:${SERVER_PORT}';
+const LIVE_URL = 'https://LucaM56.github.io';
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://LucaRM56.github.io',
+  site: BASE_URL,
   vite: {
     plugins: [tailwindcss()]
   }
